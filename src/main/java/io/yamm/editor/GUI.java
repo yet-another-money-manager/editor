54e93618656f25bc3240e846e6cbdc8d10766225
Benjamin Howe <ben@bh96.uk>
2018-06-06 21:10:10 +0100

    Added basic JavaFX GUI

--- src/main/java/io/yamm/editor/GUI.java
+++ src/main/java/io/yamm/editor/GUI.java
@@ -1,7 +1,25 @@
 package io.yamm.editor;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application  {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
